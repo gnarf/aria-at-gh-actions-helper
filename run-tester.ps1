c1f4ecdf2f1b67c3fd50e0ca4c166a964b68ba38
Mx. Corey Frang <gnarf37@gmail.com>
2025-06-11 15:12:01 -0400

    JAWS kickstart

--- run-tester.ps1
+++ run-tester.ps1
@@ -4,15 +4,28 @@ $loglocation = $pwd
 
 Write-Output "Log folder $loglocation"
 
-$nvdaParams = ""
-if ($env:RUNNER_DEBUG)
+if ($env:NVDA_PORTABLE_ZIP)
 {
-  $nvdaParams = "--debug-logging"
+  $nvdaParams = ""
+  if ($env:RUNNER_DEBUG)
+  {
+    $nvdaParams = "--debug-logging"
+  }
+  [string]$nvdaFolder = [System.IO.Path]::GetDirectoryName($env:NVDA_PORTABLE_ZIP)
+  Expand-Archive -Path "$env:NVDA_PORTABLE_ZIP" -DestinationPath "$nvdaFolder"
+  Write-Output "Starting NVDA $nvdaVersion - $nvdaFolder\$nvdaVersion\nvda.exe"
+  & "$nvdaFolder\$nvdaVersion\nvda.exe" $nvdaParams
+
+  # Spooky things... If we don't first probe the service like this, the startup of at-driver seems to fail later
+  Write-Output "Waiting for localhost:8765 to start from NVDA"
+  Wait-For-HTTP-Response -RequestURL http://localhost:8765/info
+
+  Write-Output "Starting at-driver"
+  $atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >$using:loglocation\at-driver.log }
+  Write-Output "Waiting for localhost:3031 to start from at-driver"
+  Wait-For-HTTP-Response -RequestURL http://localhost:3031
+
 }
-[string]$nvdaFolder = [System.IO.Path]::GetDirectoryName($env:NVDA_PORTABLE_ZIP)
-Expand-Archive -Path "$env:NVDA_PORTABLE_ZIP" -DestinationPath "$nvdaFolder"
-Write-Output "Starting NVDA $nvdaVersion - $nvdaFolder\$nvdaVersion\nvda.exe"
-& "$nvdaFolder\$nvdaVersion\nvda.exe" $nvdaParams
 
 # Retries to connect to an http url, allowing for any valid "response" (4xx,5xx,etc also valid)
 function Wait-For-HTTP-Response {
@@ -41,14 +54,32 @@ function Wait-For-HTTP-Response {
   Write-Output "$status after $sleeps tries"
 }
 
-# Spooky things... If we don't first probe the service like this, the startup of at-driver seems to fail later
-Write-Output "Waiting for localhost:8765 to start from NVDA"
-Wait-For-HTTP-Response -RequestURL http://localhost:8765/info
+if ($env:NVDA_PORTABLE_ZIP)
+{
+  $nvdaParams = ""
+  if ($env:RUNNER_DEBUG)
+  {
+    $nvdaParams = "--debug-logging"
+  }
+  [string]$nvdaFolder = [System.IO.Path]::GetDirectoryName($env:NVDA_PORTABLE_ZIP)
+  Expand-Archive -Path "$env:NVDA_PORTABLE_ZIP" -DestinationPath "$nvdaFolder"
+  Write-Output "Starting NVDA $nvdaVersion - $nvdaFolder\$nvdaVersion\nvda.exe"
+  & "$nvdaFolder\$nvdaVersion\nvda.exe" $nvdaParams
 
-Write-Output "Starting at-driver"
-$atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >$using:loglocation\at-driver.log }
-Write-Output "Waiting for localhost:3031 to start from at-driver"
-Wait-For-HTTP-Response -RequestURL http://localhost:3031
+    # Spooky things... If we don't first probe the service like this, the startup of at-driver seems to fail later
+  Write-Output "Waiting for localhost:8765 to start from NVDA"
+  Wait-For-HTTP-Response -RequestURL http://localhost:8765/info
+
+  Write-Output "Starting at-driver"
+  $atprocess = Start-Job -Init ([ScriptBlock]::Create("Set-Location '$pwd\nvda-at-automation\Server'")) -ScriptBlock { & .\main.exe 2>&1 >$using:loglocation\at-driver.log }
+  Write-Output "Waiting for localhost:3031 to start from at-driver"
+  Wait-For-HTTP-Response -RequestURL http://localhost:3031
+}
+
+if($env:JAWS_VERSION)
+{
+  # TODO - download/install/start JAWS here
+}
 
 switch ($env:BROWSER)
 {
@@ -77,16 +108,20 @@ switch ($env:BROWSER)
 function Trace-Logs {
   if ($env:RUNNER_DEBUG)
   {
-    Write-Output "At-Driver job process log:"
-    Receive-Job $atprocess
-    Write-Output "--at-driver.log"
-    Get-Content -Path $loglocation\at-driver.log -ErrorAction Continue
+    if ($env:NVDA_PORTABLE_ZIP)
+    {
+      Write-Output "At-Driver job process log:"
+      Receive-Job $atprocess
+      Write-Output "--at-driver.log"
+      Get-Content -Path $loglocation\at-driver.log -ErrorAction Continue
+      Write-Output "--nvda.log"
+      Get-Content -Path $env:TEMP\nvda.log -ErrorAction Continue
+    }
+    # TODO - JAWS logs?
     Write-Output "WebDriver server job process log:"
     Receive-Job $webdriverprocess
     Write-Output "--webdriver.log"
     Get-Content -Path $loglocation\webdriver.log -ErrorAction Continue
-    Write-Output "--nvda.log"
-    Get-Content -Path $env:TEMP\nvda.log -ErrorAction Continue
   }
 }
 
